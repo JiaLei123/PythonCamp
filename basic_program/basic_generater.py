30212f03d278082e4c2cfc855264cd0c8f46cc73
JiaLei <sfw123817@gmail.com>
2017-10-06 10:55:48 +0800

    rewrite init

--- basic_program/basic_generater.py
+++ basic_program/basic_generater.py
@@ -3,7 +3,11 @@
 
 class BasicGenerater(object):
     def __init__(self):
-        pass
+        self.first_name = 'Jack'
+        self.last_name = 'Freeman'
+
+    def my_name(self):
+        print '.'.join((self.first_name, self.last_name))
 
     def __del__(self):
         print "call del"
@@ -11,9 +15,17 @@ class BasicGenerater(object):
 
 class AdvaceGenerator(BasicGenerater):
     def __init__(self):
-        pass
+        BasicGenerater.__init__(self)
+        self.first_name = 'Bob'
+
+
+class AdvaceGenerator2(BasicGenerater):
+    def __init__(self):
+        super(AdvaceGenerator2, self).__init__()
+        self.first_name = 'Alon'
 
 
 if __name__ == "__main__":
-    basic = BasicGenerater()
+    basic = AdvaceGenerator2()
+    basic.my_name()
     print "end"
