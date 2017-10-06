96222b7c60964d4f8a5c03415f3c41f209db4e0c
JiaLei <sfw123817@gmail.com>
2017-10-07 00:05:58 +0800

    generator

--- basic_program/my_generator.py
+++ basic_program/my_generator.py
@@ -8,7 +8,20 @@ def flatten(nested):
             yield element
 
 
+def flatten_recursion(nested):
+    try:
+        for sublist in nested:
+            for element in flatten_recursion(sublist):
+                yield element
+    except TypeError:
+        yield nested
+
+
 if __name__ == "__main__":
     nested = [[1, 2], [3, 4], [5, 6]]
     for num in flatten(nested):
         print num
+
+    nested_1 = [[1], 2, [[3, 4, [5, 6]], 7, 8], 9]
+    for num_1 in flatten_recursion(nested_1):
+        print num_1
